e7876c98aca301644ed15f31b807406c38336b63
chen <gakiclin@gmail.com>
2017-09-13 13:32:06 +0800

    fix controllers BUG

--- controllers/movie.go
+++ controllers/movie.go
@@ -4,7 +4,6 @@ import (
 	"github.com/Luncher/go-rest/forms"
 	"github.com/Luncher/go-rest/models"
 	"github.com/gin-gonic/gin"
-	"strconv"
 )
 
 var movieModel = new(models.MovieModel)
@@ -19,28 +18,24 @@ func (user *UserController) Create(c *gin.Context) {
 		return
 	}
 
-	movieId, err := movieModel.Create(data)
+	err := movieModel.Create(data)
 	if err != nil {
 		c.JSON(406, gin.H{"message": "Movie could not be created", "error": err.Error()})
 		c.Abort()
 		return
 	}
 
-	c.JSON(200, gin.H{"message": "Movie created", "id": movieId})
+	c.JSON(200, gin.H{"message": "Movie created"})
 }
 
 func (user *UserController) Get(c *gin.Context) {
 	id := c.Param("id")
-	if id, err := strconv.ParseInt(id, 10, 64); err != nil {
-		c.JSON(404, gin.H{"message": "Invalid parameter"})
+	profile, err := movieModel.Get(id)
+	if err != nil {
+		c.JSON(404, gin.H{"message": "Movie not found", "error": err.Error()})
+		c.Abort()
 	} else {
-		profile, err := movieModel.Get(id)
-		if err != nil {
-			c.JSON(404, gin.H{"message": "Movie not found", "error": err.Error()})
-			c.Abort()
-		} else {
-			c.JSON(200, gin.H{"data": profile})
-		}
+		c.JSON(200, gin.H{"data": profile})
 	}
 }
 
